4e6933817293a662ab010805b482e75b173645c8
Adland Lee <adland@4mfd.com>
2015-09-14 15:12:23 -0700

    fix : update to current client id

--- ConferenceCentral/settings.py
+++ ConferenceCentral/settings.py
@@ -12,7 +12,7 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = '53892705221-n38m3l1jog7jf4d954dv7gn9t107c6qh.apps.googleusercontent.com'
+WEB_CLIENT_ID = '53892705221-kt14nvq614fst9fn21qlo5hp1t2p33bs.apps.googleusercontent.com'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
